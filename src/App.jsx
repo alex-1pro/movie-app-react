63d47d535a3290e5a532cfd4b78f9433074b76dc
alex-1pro <pro1.alex.b@gmail.com>
2021-07-23 11:28:20 +0300

    Creating an empty ActorsPage

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import ActorsPage from './pages/ActorsPage/ActorsPage';
+
+function App() {
+  return (
+    <div>
+      <ActorsPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/ActorsPage/ActorsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function ActorsPage() {
+    return (
+        <div>
+            Actors Page
+        </div>
+    );
+}
+
+export default ActorsPage;
